99d95dfdf694b55bb8df4bae05a8f4afd97125ac
David Wang <davidrmd@gmail.com>
2019-01-02 14:39:21 -0800

    login page and mainpage framework

---
+++ js/myfbase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCEyt_mqmUOPtDeQMwnsup8I_dKLHguXjM",
+  authDomain: "growth-983ed.firebaseapp.com",
+  databaseURL: "https://growth-983ed.firebaseio.com",
+  projectId: "growth-983ed",
+  storageBucket: "growth-983ed.appspot.com",
+  messagingSenderId: "376171377701"
+};
+firebase.initializeApp(config);
