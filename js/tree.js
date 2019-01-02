143f7bd1dbbd8fb14fa724183b1a5ee5ac402b6c
Lucy Hu <lucyhuapple@hotmail.com>
2019-01-02 14:39:56 -0800

    corrections to css

--- js/tree.js
+++ js/tree.js
@@ -1,1 +1,16 @@
-/* Canvas to draw the tree */
+/* Draw tree */
+var canvas = document.getElementById('myTree');
+var ctx = canvas.getContext('2d');
+var baseTree = new Image();
+
+baseTree.onload = function() {
+  ctx.drawImage(baseTree, 0, 0);
+};
+baseTree.src = '../img/main-tree.svg';
+
+/* Adding leaves */
+var leaves = new Image();
+leaves.onload = function() {
+	ctx.drawImage(leaves, 0, 0);
+};
+leaves.src = '../img/leaves.svg';
