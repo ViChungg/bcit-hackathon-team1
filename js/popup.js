005369a1864b5c145b549497f02c3ad4dfbc147d
Lambert Chan <lambert.lh.chan@gmail.com>
2019-01-02 12:08:31 -0800

    attempt to connect the files

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,20 @@
 var popup = document.getElementById('popup1');
 
-var btn = document.getElementById("button1");
+var btn1 = document.getElementById("educationButton");
+var btn2 = document.getElementById("fitnessButton");
+var btn3 = document.getElementById("healthButton");
 
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
+btn1.onclick = function () {
+  popup.style.display = "block";
+}
+
+btn2.onclick = function() {
+  popup.style.display = "block";
+}
+
+btn3.onclick = function() {
   popup.style.display = "block";
 }
 
