ff8010fe4037d5cba009d8f753271447abea26b1
David Wang <davidrmd@gmail.com>
2019-01-03 12:40:10 -0800

    final version for presentation

--- js/add-user.js
+++ js/add-user.js
@@ -3,35 +3,35 @@ function newUserDatabase() {
         firebase.database().ref("users/" + user.uid).update({
             "name": user.displayName,
             "email": user.email,
-            "eduPoints": 0,
-            "fitPoints": 0,
-            "healthPoints": 0,
+            "eduPoints": "0",
+            "fitPoints": "0",
+            "healthPoints": "0",
         });
     })
 }
 
 // adds user to database
-(function(){
+// (function(){
 
-    var firebase = app_firebase;
-    // for the currently authenticated firebase user
-    // create your own "user" node in the datebase using 
-    // the firebase generated "uid"
+//     var firebase = app_firebase;
+//     // for the currently authenticated firebase user
+//     // create your own "user" node in the datebase using 
+//     // the firebase generated "uid"
     
-    firebase.auth().onAuthStateChanged(function(user){
-        if (firebase.auth().currentUser.metadata.creationTime 
-        == firebase.auth().currentUser.metadata.lastSignInTime) {
-            firebase.database().ref("users/"+user.uid).update(
-            {
-             "name":user.displayName, 
-             "email":user.email,
-             "eduPoints":0,
-             "fitPoints":0,
-             "healthPoints":0
-            });
-        }
+//     firebase.auth().onAuthStateChanged(function(user){
+//         if (firebase.auth().currentUser.metadata.creationTime 
+//         == firebase.auth().currentUser.metadata.lastSignInTime) {
+//             firebase.database().ref("users/"+user.uid).update(
+//             {
+//              "name":user.displayName, 
+//              "email":user.email,
+//              "eduPoints":0,
+//              "fitPoints":0,
+//              "healthPoints":0
+//             });
+//         }
         
-    });
+//     });
     
     
-})()
+// })()
