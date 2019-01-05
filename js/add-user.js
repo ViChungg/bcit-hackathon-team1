1a3da201dbd026f59c4ac05d2cecd3f9938a5f16
Lucy Hu <lucyhuapple@hotmail.com>
2019-01-04 23:52:04 -0800

    New users can now be added to the database

--- js/add-user.js
+++ js/add-user.js
@@ -1,37 +1,43 @@
-function newUserDatabase() {
-    firebase.auth().onAuthStateChanged(function(user) {
-        firebase.database().ref("users/" + user.uid).update({
-            "name": user.displayName,
-            "email": user.email,
-            "eduPoints": "0",
-            "fitPoints": "0",
-            "healthPoints": "0",
-        });
-    })
-}
+		function newUserDatabase() {
 
-// adds user to database
-// (function(){
+			firebase.auth().onAuthStateChanged(function (user) {
 
-//     var firebase = app_firebase;
-//     // for the currently authenticated firebase user
-//     // create your own "user" node in the datebase using 
-//     // the firebase generated "uid"
-    
-//     firebase.auth().onAuthStateChanged(function(user){
-//         if (firebase.auth().currentUser.metadata.creationTime 
-//         == firebase.auth().currentUser.metadata.lastSignInTime) {
-//             firebase.database().ref("users/"+user.uid).update(
-//             {
-//              "name":user.displayName, 
-//              "email":user.email,
-//              "eduPoints":0,
-//              "fitPoints":0,
-//              "healthPoints":0
-//             });
-//         }
-        
-//     });
-    
-    
-// })()
+				var userId = firebase.auth().currentUser.uid;
+
+				firebase.database().ref("users/" + userId).update({
+					"name": user.displayName,
+					"email": user.email,
+					"eduPoints": "0",
+					"fitPoints": "0",
+					"healthPoints": "0",
+				});
+			})
+		}
+
+
+
+		// //adds user to database
+		// (function(){
+		//
+		//     var firebase = app_firebase;
+		//     // for the currently authenticated firebase user
+		//     // create your own "user" node in the datebase using 
+		//     // the firebase generated "uid"
+		//    
+		//     firebase.auth().onAuthStateChanged(function(user){
+		//         if (firebase.auth().currentUser.metadata.creationTime 
+		//         == firebase.auth().currentUser.metadata.lastSignInTime) {
+		//             firebase.database().ref("users/"+user.uid).update(
+		//             {
+		//              "name":user.displayName, 
+		//              "email":user.email,
+		//              "eduPoints":0,
+		//              "fitPoints":0,
+		//              "healthPoints":0
+		//             });
+		//         }
+		//        
+		//     });
+		//    
+		//    
+		// })()
